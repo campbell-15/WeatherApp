df17c291d0c2c76574691f5cbf92d3cfb3ab9fc7
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-05-03 14:38:38 +0200

    added components

---
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WeatherApp.css';
+
+const WeatherApp = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default WeatherApp

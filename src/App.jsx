4405cbcdd55446ba1d41b532740c74f43f4b5c02
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-05-03 19:49:11 +0200

    reworked routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WeatherApp from './Components/WeatherApp/WeatherApp.jsx'
+import StatsPage from './Components/StatsPage/StatsPage.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <WeatherApp/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<WeatherApp />} />
+        <Route path='/weather-stats' element={<StatsPage/>} />
+      </Routes>
+    </Router>
   );
 }
 

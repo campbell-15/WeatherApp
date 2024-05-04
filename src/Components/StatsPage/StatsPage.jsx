561731c3f6ca991ed6d8eb5b9f6cf1a4b8a846b0
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-05-04 22:43:55 +0200

    added forecast content

--- src/Components/StatsPage/StatsPage.jsx
+++ src/Components/StatsPage/StatsPage.jsx
@@ -14,7 +14,7 @@ const StatsPage = () => {
                 )}
             </div>
 
-            <div className="stats">
+            {/* <div className="stats">
                 {weatherData && (
                 <div>
                     <p className='stat-item'>Temperature: {weatherData.main.temp} °C</p>
@@ -22,6 +22,95 @@ const StatsPage = () => {
                     <p className='stat-item'>Wind Speed: {weatherData.wind.speed} km/h</p>
                 </div>
             )}
+            </div> */}
+
+            <div class="ag-format-container">
+                <div class="ag-courses_box">
+
+                    <div class="ag-courses_item">
+                        <div class="ag-courses-item_link">
+                            <div class="ag-courses-item_bg"></div>
+
+                            <div class="ag-courses-item_title">
+                            Tempreture:
+                            </div>
+
+                            <div class="ag-courses-item_date-box">
+                            {weatherData.main.temp} °C
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="ag-courses_item">
+                        <div class="ag-courses-item_link">
+                            <div class="ag-courses-item_bg"></div>
+
+                            <div class="ag-courses-item_title">
+                            Weather
+                            </div>
+
+                            <div class="ag-courses-item_date-box">
+                            {weatherData.weather[0].main}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="ag-courses_item">
+                        <div class="ag-courses-item_link">
+                            <div class="ag-courses-item_bg"></div>
+
+                            <div class="ag-courses-item_title">
+                            Feels Like
+                            </div>
+
+                            <div class="ag-courses-item_date-box">
+                            {weatherData.main.feels_like}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="ag-courses_item">
+                        <div class="ag-courses-item_link">
+                            <div class="ag-courses-item_bg"></div>
+
+                            <div class="ag-courses-item_title">
+                            Longitude
+                            </div>
+
+                            <div class="ag-courses-item_date-box">
+                            {weatherData.coord.lon} °
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="ag-courses_item">
+                        <div class="ag-courses-item_link">
+                            <div class="ag-courses-item_bg"></div>
+
+                            <div class="ag-courses-item_title">
+                            Latitude
+                            </div>
+
+                            <div class="ag-courses-item_date-box">
+                            {weatherData.coord.lat} °
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="ag-courses_item">
+                        <div class="ag-courses-item_link">
+                            <div class="ag-courses-item_bg"></div>
+
+                            <div class="ag-courses-item_title">
+                            Pressure
+                            </div>
+
+                            <div class="ag-courses-item_date-box">
+                                {weatherData.main.pressure} hPa
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 

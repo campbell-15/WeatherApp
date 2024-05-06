35970451cd4ac865e54b6f40bee20e9cdaa5c3b2
Campbell Davies-Webb <campbelldavieswebb@gmail.com>
2024-05-06 17:08:41 +0200

    added °c to values

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -83,7 +83,7 @@ const WeatherApp = () => {
             <div className="weather-image">
                 <img src={wicon} alt="" />
             </div>
-            <div className="weather-temp">24</div>
+            <div className="weather-temp">24 °c</div>
             <div className="weather-location">London</div>
             <div className="data-container">
                 <div className="element">
